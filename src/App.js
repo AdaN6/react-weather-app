a4eb35e08dbe9710cb7b02dbdcc786dd8da89c5b
AdaN6 <a_disney_da@hotmail.com>
2022-11-14 22:45:46 +0100

    added github repo url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@
 function App() {
   return (
     <div>
-    Hello
+      Hello
+      <footer>
+        This project was coded by Ada Neo and is open-sourced
+        <a href="https://github.com/AdaN6/react-weather-app" target="_blank">open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
