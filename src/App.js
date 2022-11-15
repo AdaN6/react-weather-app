80c2be71613d0aab6c25162ba23c6d87442707f4
AdaN6 <a_disney_da@hotmail.com>
2022-11-15 20:05:59 +0100

    npm install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 
-
 function App() {
   return (
-    <div>
-      <button className="btn btn-primary">Hey</button>
-      Hello
-      <footer>
-        This project was coded by Ada Neo and is open-sourced 
-        <a href="https://github.com/AdaN6/react-weather-app" target="_blank"> open-sourced on GitHub</a>
-      </footer>
+    <div className="App">
+      <div className="Container">
+        <button className="btn btn-primary"> Hello </button>
+        Hello
+        <footer>
+          This project was coded by Ada Neo and is open-sourced
+          <a href="https://github.com/AdaN6/react-weather-app" target="_blank">
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
